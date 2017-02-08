ef292593629ff9def7294f708cfc2aaddd654397
Asim Hussain <jawache@gmail.com>
2017-02-08 23:08:35 +0000

    Step-6 Book List Complete

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,4 +1,5 @@
-import {Component, OnInit} from "@angular/core";
+import {Component, OnInit, Input} from "@angular/core";
+import {Book} from "../shared/book";
 
 @Component({
   selector: 'app-book-list',
@@ -6,6 +7,9 @@ import {Component, OnInit} from "@angular/core";
   styleUrls: ['./book-list.component.css']
 })
 export class BookListComponent implements OnInit {
+
+  @Input()
+  private books: Book[];
 
   ngOnInit() {
   }
