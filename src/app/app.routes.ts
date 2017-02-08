26712560131cae317d18273d22baa0da1dbeec8b
Asim Hussain <jawache@gmail.com>
2017-02-08 22:03:51 +0000

    Step-2 Routing Complete

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,11 @@
 import {Routes} from "@angular/router";
+import {LibraryComponent} from "./library/library.component";
+import {SearchComponent} from "./search/search.component";
+import {BookComponent} from "./book/book.component";
 
 export const routes: Routes = [
-  //TODO: Define the routes
+  {path: '', redirectTo:'library', pathMatch:'full'},
+  {path: 'library', component: LibraryComponent},
+  {path: 'search', component: SearchComponent},
+  {path: 'book/:bookId', component: BookComponent},
 ];
